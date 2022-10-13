b7613bc01c67173db56f400737a7cefaf4804d0e
pallavipb2748.doc@gmail.com <pallavipb2748.doc@gmail.com>
2022-10-13 19:21:20 +0900

    1st commit, Main app and add invoice example

---
+++ Invoice_main/Invoice_main/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Invoice_main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class DataObject
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+            public int C { get; set; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            var list = new ObservableCollection<DataObject>();
+            list.Add(new DataObject() { A = 6, B = 7, C = 5 });
+            list.Add(new DataObject() { A = 5, B = 8, C = 4 });
+            list.Add(new DataObject() { A = 4, B = 3, C = 0 });
+            this.dataGrid1.ItemsSource = list;
+        }
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            SubWindow subWindow = new SubWindow();
+            subWindow.Show();
+        }
+    }
+}
